c5fea1abe062d6242a33d02de51f83a74c605046
ginopastran <ginopastran@gmail.com>
2024-03-30 16:11:30 -0300

    adding keys

--- class/config.php
+++ class/config.php
@@ -1,14 +1,14 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+define('DB_NAME', 'cs2');
+define('DB_USER', 'root');
+define('DB_PASS', 'pC53.D!I(bUi_3*X');
 
 define('WEB_STYLE_DARK', true);
 
-define('STEAM_API_KEY', '');
-define('STEAM_DOMAIN_NAME', '');
+define('STEAM_API_KEY', '8679787B13B4A3B83CCAD201E676DEDD');
+define('STEAM_DOMAIN_NAME', 'AAAAA');
 define('STEAM_LOGOUT_PAGE', '');
 define('STEAM_LOGIN_PAGE', '');
 
