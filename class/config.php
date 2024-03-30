7ca740db98785e8c75bfdd2e81f47c071c887148
ginopastran <ginopastran@gmail.com>
2024-03-30 19:23:24 -0300

    .env and aiven keys

--- class/config.php
+++ class/config.php
@@ -1,14 +1,14 @@
 <?php
-define('DB_HOST', 'roundhouse.proxy.rlwy.net');
-define('DB_PORT', '32346');
-define('DB_NAME', 'railway');
-define('DB_USER', 'root');
-define('DB_PASS', 'WUGnRkOreiBTdSyRnZRtYSOabBqZRucA');
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_PORT', '22420');
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USERNAME']);
+define('DB_PASS', $_ENV['DB_PASSWORD']);
 
 define('WEB_STYLE_DARK', true);
 
-define('STEAM_API_KEY', '8679787B13B4A3B83CCAD201E676DEDD');
-define('STEAM_DOMAIN_NAME', 'AAAAA');
-define('STEAM_LOGOUT_PAGE', 'https://cs2wswesite-production.up.railway.app/index.php');
-define('STEAM_LOGIN_PAGE', 'https://cs2wswesite-production.up.railway.app/index.php');
+define('STEAM_API_KEY', 'B67D20B29A754AAF859336120B03E82C');
+define('STEAM_DOMAIN_NAME', 'https://cs2wswesite-production.up.railway.app/');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
 
