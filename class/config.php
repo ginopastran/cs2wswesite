3e161a980b4c6377a3f251170a0a9cd9f2379bba
ginopastran <ginopastran@gmail.com>
2024-03-30 19:57:00 -0300

    PORT ENV ADDED

--- class/config.php
+++ class/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_PORT', '22420');
+define('DB_PORT', $_ENV['DB_PORT']);
 define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_USER', $_ENV['DB_USERNAME']);
 define('DB_PASS', $_ENV['DB_PASSWORD']);
